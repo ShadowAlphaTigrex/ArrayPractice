6520c02e963e6a5a86690f69cf7856395ee079f1
Aleeya Noradzmudin <18208@bayfield-high.school.nz>
2020-05-20 11:49:16 +1200

    Add project files.

---
+++ ArrayPractice/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ArrayPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.lblOutput1 = new System.Windows.Forms.Label();
+            this.lblOutput2 = new System.Windows.Forms.Label();
+            this.lblFruit = new System.Windows.Forms.Label();
+            this.btnShow2 = new System.Windows.Forms.Button();
+            this.lblTotalFruit = new System.Windows.Forms.Label();
+            this.lblTotalNum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(127, 151);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // lblOutput1
+            // 
+            this.lblOutput1.AutoSize = true;
+            this.lblOutput1.Location = new System.Drawing.Point(124, 41);
+            this.lblOutput1.Name = "lblOutput1";
+            this.lblOutput1.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput1.TabIndex = 1;
+            // 
+            // lblOutput2
+            // 
+            this.lblOutput2.AutoSize = true;
+            this.lblOutput2.Location = new System.Drawing.Point(124, 88);
+            this.lblOutput2.Name = "lblOutput2";
+            this.lblOutput2.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput2.TabIndex = 2;
+            // 
+            // lblFruit
+            // 
+            this.lblFruit.AutoSize = true;
+            this.lblFruit.Location = new System.Drawing.Point(366, 63);
+            this.lblFruit.Name = "lblFruit";
+            this.lblFruit.Size = new System.Drawing.Size(0, 13);
+            this.lblFruit.TabIndex = 3;
+            // 
+            // btnShow2
+            // 
+            this.btnShow2.Location = new System.Drawing.Point(369, 151);
+            this.btnShow2.Name = "btnShow2";
+            this.btnShow2.Size = new System.Drawing.Size(75, 23);
+            this.btnShow2.TabIndex = 4;
+            this.btnShow2.Text = "Show";
+            this.btnShow2.UseVisualStyleBackColor = true;
+            this.btnShow2.Click += new System.EventHandler(this.btnShow2_Click);
+            // 
+            // lblTotalFruit
+            // 
+            this.lblTotalFruit.AutoSize = true;
+            this.lblTotalFruit.Location = new System.Drawing.Point(366, 110);
+            this.lblTotalFruit.Name = "lblTotalFruit";
+            this.lblTotalFruit.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalFruit.TabIndex = 5;
+            // 
+            // lblTotalNum
+            // 
+            this.lblTotalNum.AutoSize = true;
+            this.lblTotalNum.Location = new System.Drawing.Point(124, 121);
+            this.lblTotalNum.Name = "lblTotalNum";
+            this.lblTotalNum.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalNum.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 261);
+            this.Controls.Add(this.lblTotalNum);
+            this.Controls.Add(this.lblTotalFruit);
+            this.Controls.Add(this.btnShow2);
+            this.Controls.Add(this.lblFruit);
+            this.Controls.Add(this.lblOutput2);
+            this.Controls.Add(this.lblOutput1);
+            this.Controls.Add(this.btnShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label lblOutput1;
+        private System.Windows.Forms.Label lblOutput2;
+        private System.Windows.Forms.Label lblFruit;
+        private System.Windows.Forms.Button btnShow2;
+        private System.Windows.Forms.Label lblTotalFruit;
+        private System.Windows.Forms.Label lblTotalNum;
+    }
+}
+
---
+++ ArrayPractice/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            int[] num = { 8, 9, 5, 6, 2, 8, 4 };
+            lblOutput1.Text = "First element is " + num[0];
+            lblOutput2.Text = "Fifth element is " + num[4];
+
+            int total = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                total = total + num[i];
+            }
+            int aftertotal;
+            aftertotal = total * 2;
+            lblTotalNum.Text = "Total is " + aftertotal;
+        }
+
+        private void btnShow2_Click(object sender, EventArgs e)
+        {
+            string[] fruit = { "Banana", "apple", "pear", "orange", "kiwi fruit" };
+            fruit[1] = "strawberry";
+            // changes the second element // 
+            lblFruit.Text = "Second element is " + fruit[1];
+
+            int numOfFruit;
+            numOfFruit = fruit.Length;            lblTotalFruit.Text = "Total fruit is " + numOfFruit;
+            //Adding all element//
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
